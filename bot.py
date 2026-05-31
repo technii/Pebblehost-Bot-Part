248af743cb42e229c02c4c1734041715e369dbcb
techni <145453745+technii@users.noreply.github.com>
2026-05-31 18:02:12 +0100

    changed some stuff :)

--- bot.py
+++ bot.py
@@ -8,18 +8,22 @@ import requests
 import json
 import mysql.connector
 import nacl
+from mutagen.mp3 import MP3
 jsonfile = open("token.json")
 jsondict : dict = json.load(jsonfile)
-
+allowedcontenttypes = ["audio/mpeg"]
 intents = discord.Intents.default()
 openvoiceclients = {}
+audiofp = "Sounds/"
+sqlforuploadingsounds = "INSERT INTO SOUNDPOINTERS (GUILDID,FILENAME,LENGTH,USERID,FileID) VALUES (%s,%s,%s,%s,%s) "
 
 db = mysql.connector.connect(
     host=jsondict.get("SQLHost"),
     port = jsondict.get("SQLPort"),
     user = jsondict.get("SQLUsername"),
     password = jsondict.get("SQLPassword"),
-    database = jsondict.get("SQLUsername")
+    database = jsondict.get("SQLUsername"),
+    connection_timeout = 10
 )
 sqlcursor = db.cursor()
 
@@ -56,15 +60,28 @@ async def _leavevc(interaction : discord.Interaction):
 
 @client.tree.command(name="uploadsound")
 @app_commands.allowed_contexts(guilds=True)
-async def _uploadsound(interaction : discord.Interaction):
+async def _uploadsound(interaction : discord.Interaction, sound : discord.Attachment):
     try:
-        pass
+        if sound.content_type in allowedcontenttypes:
+            sqlcursor.execute("SELECT `FileID` FROM SOUNDPOINTERS ORDER BY FileID DESC LIMIT 1")
+            currentfileid : int  = int(str(sqlcursor.fetchone()).removeprefix("(").removesuffix(",)")) + 1
+            await sound.save(fp=audiofp + str(interaction.guild.id) + str(currentfileid)+ ".mp3")
+            vals = (str(interaction.guild.id),sound.filename,str(MP3(audiofp + str(interaction.guild.id) + str(currentfileid)+ ".mp3").info.length),str(interaction.user.id),currentfileid)
+            print(vals)
+            
+            sqlcursor.execute(sqlforuploadingsounds,vals)
+            db.commit()
+            await interaction.response.send_message("SAVED", ephemeral=True)
+            
+        else:
+            await interaction.response.send_message("Must be a .mp3", ephemeral=True)
+
     except Exception as e:
         await interaction.response.send_message(e,ephemeral=True)
 
 @client.tree.command(name="playsound")
 @app_commands.allowed_contexts(guilds=True)
-async def _playsound(interaction : discord.Interaction):
+async def _playsound(interaction : discord.Interaction, soundname: str):
     try:
         pass
     except Exception as e:
@@ -74,9 +91,11 @@ async def _playsound(interaction : discord.Interaction):
 @app_commands.allowed_contexts(guilds=True)
 async def _soundlist(interaction : discord.Interaction):
     try:
-        pass
+        print(currentfileid)
     except Exception as e:
         await interaction.response.send_message(e,ephemeral=True)
+
+
 
 def run_bot():
     client.run(jsondict.get("token"))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bot import run_bot
 from waitress import serve
 from flaskapp import app
 
-#bot_thread = threading.Thread(target=run_bot, daemon=True)
-#bot_thread.start()
+bot_thread = threading.Thread(target=run_bot, daemon=True)
+bot_thread.start()
 
 serve(app, host="0.0.0.0", port=8054) 
