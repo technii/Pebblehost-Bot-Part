330d68289dffd0a61a60cca813353ce8e617ba53
techni <145453745+technii@users.noreply.github.com>
2026-06-01 16:11:18 +0100

    testing something

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from flaskapp import app
 
 bot_thread = threading.Thread(target=run_bot, daemon=True)
 bot_thread.start()
+print("Started the server on 0.0.0.0 at port 8054")
 
 serve(app, host="0.0.0.0", port=8054) 
